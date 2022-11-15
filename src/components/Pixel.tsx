60c219d6b4f3163e9e4e41ff67748eed712d526c
Paul Tiry <tirypaul@gmail.com>
2022-11-15 17:35:24 +0100

    changed hover functionality

--- src/components/Pixel.tsx
+++ src/components/Pixel.tsx
@@ -1,24 +1,40 @@
-import { useContext, useRef, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import styled from 'styled-components'
 import ColorSelected from '../context/ColorSelected.context'
 import useNeighbours from '../hooks/useNeighbours'
 
 const Pixel = ({ width, id }: { width: number; id: string }): JSX.Element => {
   const { colorSelected } = useContext(ColorSelected)
-  const [color, setColor] = useState('#ffffff')
+  const [color, setColor] = useState<string>('#ffffff')
+  const [prevColor, setPrevColor] = useState<string>(color)
   const pixel = useRef<any>(undefined)
   const { findNeihbours } = useNeighbours()
 
   const handleClick = () => {
-    const tool = 'fill'
+    /* const tool = 'fill'
     const pixelColor = pixel.current.style.backgroundColor
 
     if (tool === 'fill') {
       findNeihbours(pixel?.current).forEach(el => {
         el.setAttribute('style', `background-color: ${pixelColor};`)
       })
-    }
-    // setColor(colorSelected)
+    } */
+    setColor(colorSelected)
+    setPrevColor(colorSelected)
+  }
+
+  const changeOnHover = () => {
+    setColor((prev: string) => {
+      setPrevColor(prev)
+      return colorSelected
+    })
+  }
+
+  const resetColor = () => {
+    setColor((prev: string) => {
+      setPrevColor(prev)
+      return prevColor
+    })
   }
 
   return (
@@ -27,23 +43,15 @@ const Pixel = ({ width, id }: { width: number; id: string }): JSX.Element => {
       id={id}
       width={width}
       style={{ backgroundColor: color }}
-      selected={colorSelected}
+      onMouseEnter={changeOnHover}
+      onMouseLeave={resetColor}
       onClick={handleClick}
     ></Pix>
   )
 }
 export default Pixel
 
-const Pix = styled.div.attrs(
-  ({ background, selected, width }: { background: string; selected: string; width: number }) => ({
-    background,
-    selected,
-    width,
-  })
-)`
+const Pix = styled.div<{ width: number }>`
   border: 0.5px solid #000000;
   width: calc(${({ width }) => `550px / ${width}`});
-  :hover {
-    background-color: ${({ selected }) => selected};
-  }
 `
